9b48ef1975c4d81e33a2081e8e41d06c6d941d50
Bilalkhatri <bilal.khatri@yahoo.com>
2021-11-27 20:22:52 +0530

    profile setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -18,7 +18,8 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased bg-light">
+    <body class="font-sans hold-transition dark-skin sidebar-mini theme-primary fixed">
+        <div class="wrapper">
         <x-jet-banner />
         @livewire('navigation-menu')
 
@@ -30,14 +31,149 @@
         </header>
 
         <!-- Page Content -->
-        <main class="container my-5">
+        <div class="content-wrapper">
+            <div class="container-full">
             {{ $slot }}
-        </main>
+        </div>
+    </div>
+    <!-- /.content-wrapper -->
 
         @stack('modals')
 
         @livewireScripts
 
         @stack('scripts')
+    </div><!-- ./wrapper -->
     </body>
+</html> --}}
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    {{-- <link rel="icon" href="../images/favicon.ico"> --}}
+
+     <!-- Styles -->
+     <link rel="stylesheet" href="{{ asset('panel/css/style.css') }}">
+
+
+     <link rel="stylesheet" href="{{ asset('panel/css/skin_color.css') }}">
+     <link rel="stylesheet" href="{{ asset('panel/css/animate.css') }}">
+     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+     @livewireStyles
+
+     <!-- Scripts -->
+     <script src="{{ asset('js/app.js') }}" defer></script>
+
+
+  </head>
+
+<body class="hold-transition dark-skin sidebar-mini theme-primary fixed">
+
+<div class="wrapper">
+
+  <header class="main-header">
+    <!-- Header Navbar -->
+    <nav class="navbar navbar-static-top pl-30">
+      <!-- Sidebar toggle button-->
+	  <div>
+		  <ul class="nav">
+			<li class="btn-group nav-item">
+				<a href="#" class="waves-effect waves-light nav-link rounded svg-bt-icon" data-toggle="push-menu" role="button">
+					<i class="nav-link-icon mdi mdi-menu"></i>
+			    </a>
+			</li>
+			<li class="btn-group nav-item">
+				<a href="#" data-provide="fullscreen" class="waves-effect waves-light nav-link rounded svg-bt-icon" title="Full Screen">
+					<i class="nav-link-icon mdi mdi-crop-free"></i>
+			    </a>
+			</li>
+
+		  </ul>
+	  </div>
+
+      <div class="navbar-custom-menu r-side">
+        <ul class="nav navbar-nav">
+		  <!-- full Screen -->
+	      <li class="search-bar">
+			  <div class="lookup lookup-circle lookup-right">
+			     <input type="text" name="s">
+			  </div>
+		  </li>
+
+
+	      <!-- User Account-->
+          <li class="dropdown user user-menu">
+			<a href="#" class="waves-effect waves-light rounded dropdown-toggle p-0" data-toggle="dropdown" title="User">
+				<img src="../images/avatar/1.jpg" alt="">
+			</a>
+			<ul class="dropdown-menu animated flipInX">
+			  <li class="user-body">
+				 <a class="dropdown-item" href="#"><i class="ti-user text-muted mr-2"></i> Profile</a>
+				 <a class="dropdown-item" href="#"><i class="ti-wallet text-muted mr-2"></i> My Wallet</a>
+				 <a class="dropdown-item" href="#"><i class="ti-settings text-muted mr-2"></i> Settings</a>
+				 <div class="dropdown-divider"></div>
+				 <a class="dropdown-item" href="#"><i class="ti-lock text-muted mr-2"></i> Logout</a>
+			  </li>
+			</ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Left side column. contains the logo and sidebar -->
+   <x-panel.mainSidebar/>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+	  <div class="container-full">
+
+		<!-- Main content -->
+        @isset($header)
+        {{ $header }}
+        @endisset
+        {{ $slot }}
+
+		<!-- /.content -->
+	  </div>
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+		  <li class="nav-item">
+			<a class="nav-link" href="javascript:void(0)">FAQ</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="#">Purchase Now</a>
+		  </li>
+		</ul>
+    </div>
+	  &copy; 2020 <a href="#">Psd to Html Expert</a>. All Rights Reserved.
+  </footer>
+
+
+<!-- Vendor JS -->
+<script src="{{ asset('panel/js/vendors.min.js') }}"></script>
+
+
+<!-- Sunny Admin App -->
+ <script src="{{ asset('panel/js/template.js') }}"></script>
+{{-- <script src="js/pages/dashboard.js"></script> --}}
+
+  @stack('modals')
+
+  @livewireScripts
+
+  @stack('scripts')
+</div>
+<!-- ./wrapper -->
+</body>
 </html>
--- resources/views/livewire/panel/admin/users.blade.php
+++ resources/views/livewire/panel/admin/users.blade.php
@@ -1,11 +1,28 @@
 
     <x-slot name="header">
-        <h2 class="h4 font-weight-bold">
-            {{ __('Users') }}
-        </h2>
+        <div class="content-header">
+            <div class="d-flex align-items-center">
+                <div class="mr-auto">
+                    <h3 class="page-title">Users</h3>
+                    <div class="d-inline-block align-items-center">
+                        <nav>
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
+                                <li class="breadcrumb-item" aria-current="page">Extra</li>
+                                <li class="breadcrumb-item active" aria-current="page">Profile</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
     </x-slot>
+<section class="content">
 
-    <div>
-     <h1>this is users table</h1>
-    </div>
+        <div class="row">
+            <div class="col-12 col-lg-5 col-xl-4">
+            <h1>this is users table</h1>
+            </div>
+        </div>
+</section>
 
